e7739a4ea62d6db82c6309d4c182628612c2c1bc
Alex Piontek <nekom113@mail.ru>
2023-06-10 11:21:22 +0300

    change DB connection address

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { PORT = 3000 } = process.env;
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mestodb', {});
+    await mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
   } catch (err) {
     console.error(`Catch ${err}`);
   }
